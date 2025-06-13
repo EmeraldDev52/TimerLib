80504732ecfb42f20fb86b11cf463fd69f62191e
EmeraldWeb52 <brooks.github@gmail.com>
2025-06-13 18:38:35 -0500

    initial commit

---
+++ include/timer.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <chrono>
+#include <thread>
+
+
+namespace Timers {
+
+    // timer that can check elapsed time at any moment. less precise
+    struct Timer {
+    public:
+        Timer();
+
+        void reset();
+
+        double elapsedSeconds() const;
+
+        double elapsedMilliseconds() const;
+
+        double elapsedMicroseconds() const;
+    private:
+        std::chrono::high_resolution_clock::time_point m_start;
+    };
+
+
+    // scope based timer that calculates based on the lifetime of the object. more precise
+    struct ScopedTimer {
+        ScopedTimer(double* outDuration);
+        ~ScopedTimer();
+
+    private:
+        double* durationRef;
+        std::chrono::high_resolution_clock::time_point m_start;
+    };
+}
---
+++ src/timer.cpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <chrono>
+#include <thread>
+#include "../include/timer.h"
+
+namespace Timers {
+
+    // timer that can check elapsed time at any moment. less precise
+    Timer::Timer() {
+        m_start = std::chrono::high_resolution_clock::now();
+    }
+
+    void Timer::reset() {
+        m_start = std::chrono::high_resolution_clock::now();
+    }
+
+    double Timer::elapsedSeconds() const {
+        return std::chrono::duration<double>(std::chrono::steady_clock::now() - m_start).count();
+    }
+    
+    double Timer::elapsedMilliseconds() const {
+        return std::chrono::duration<double, std::milli>(std::chrono::steady_clock::now() - m_start).count();
+    }
+
+    double Timer::elapsedMicroseconds() const {
+        return std::chrono::duration<double, std::micro>(std::chrono::steady_clock::now() - m_start).count();
+    }
+
+
+
+    // scope based timer that calculates based on the lifetime of the object. more precise
+    ScopedTimer::ScopedTimer(double* outDuration = nullptr)
+        : m_start(std::chrono::steady_clock::now()), durationRef(outDuration) {}
+
+    ScopedTimer::~ScopedTimer() {
+        auto end = std::chrono::steady_clock::now();
+        std::chrono::duration<double, std::milli> duration = end - m_start;
+        if (durationRef != nullptr) {
+            *durationRef = duration.count();
+        }
+    }
+}
